cc92552497a0c8e5b21ba038efde05cf20192125
Kai He <hekai_aggie2013@163.com>
2024-09-18 12:23:33 +0800

    use constructor syntax sugar

--- lib/question.dart
+++ lib/question.dart
@@ -2,8 +2,10 @@ class Question {
   String questionText = '';
   bool questionAnswer = false;
 
-  Question(String q, bool a) {
-    this.questionText = q;
-    this.questionAnswer = a;
-  }
+  // Question(String q, bool a) {
+  //   this.questionText = q;
+  //   this.questionAnswer = a;
+  // }
+
+  Question(this.questionText, this.questionAnswer);
 }
